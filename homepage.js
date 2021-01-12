596d61f5ebb2f882b9df6f7a5850cde8dbc1584c
AKhaz <aiden.khazeni.22@bishops.com>
2021-01-12 12:01:29 -1000

    New Input Tags and Site Layout

--- homepage.js
+++ homepage.js
@@ -1,8 +1,48 @@
-function doSomething(){
-  var username = document.getElementById('UserTag').value;
-  var title = document.getElementById('Title');
-  var desc = document.getElementById('Desc');
-  console.log(username);
-  title.innerHTML = "Done! Click Again.";
-  desc.innerHTML = username;
+function postReview(){
+  var reviews = document.getElementById("Reviews")
+  var userReview = document.getElementById("UserReview").value;
+  reviews.innerHTML = "\n";
+  reviews.innerHTML += "<h3>Anonymous</h3>\n";
+  reviews.innerHTML += "<p>" + userReview + "</p>";
+
+  localStorage.setItem("lastReviewTest", userReview);
+}
+
+function loadReview(){
+  var reviews = document.getElementById("Reviews")
+  var userReview = localStorage.getItem("lastReviewTest");
+  reviews.innerHTML = "\n";
+  reviews.innerHTML += "<h3>Anonymous</h3>\n";
+  reviews.innerHTML += "<p>" + userReview + "</p>";
+}
+
+function changeShippingOption(){
+  var shippingOption = document.getElementById("ShippingOption");
+  var optionDiv = document.getElementById("OtherSelections");
+
+  if (shippingOption.checked){
+    optionDiv.innerHTML += "<p>Unfortunately, we cannot complete this request. This product and company doesn't exist.</p>"
+  }
+}
+
+function changeProductType(){
+  var image = document.getElementById('ProductImage')
+  smallOption = document.getElementById("Small");
+  mediumOption = document.getElementById("Medium");
+  largeOption = document.getElementById("Large");
+
+  if (smallOption.checked){
+    image.src = "images/box2.jpg";
+  }
+  else if (mediumOption.checked){
+    image.src = "images/box1.jpg";
+  }
+  else if (largeOption.checked){
+    image.src = "images/box3.jpg";
+  }
+
+
+  reviews.innerHTML = "\n";
+  reviews.innerHTML += "<h3>Anonymous</h3>\n";
+  reviews.innerHTML += "<p>" + userReview + "</p>";
 }
